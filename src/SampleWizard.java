dc5fb2608e689405c50c74b5a9fd4c9990c10d7d
Callum Robertson <callumullac@hotmail.com>
2017-03-18 21:18:56 +1100

    Initial commit

---
+++ src/SampleWizard.java
@@ -0,0 +1,14 @@
+import com.github.crob1140.codewiz.Wizard;
+
+public class SampleWizard extends Wizard {
+	
+	@Override
+	public void onIdle() {
+		
+	}
+	
+	@Override
+	public void onEnemySighted() {
+		
+	}
+}
